f9362b44b3e1159ed77db708211663d1f18aac80
a.lozyuk <a.lozyuk@pixelplex.io>
2021-10-21 01:43:02 +0300

    fix

--- src/components/company-name/index.tsx
+++ src/components/company-name/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { PROJECT_NAME } from '@constants/global.constants';
 import styles from './company-name.module.scss';
 
-
 export const CompanyName: React.FC = () => (
   <div className={styles.companyName}>
     {PROJECT_NAME}
