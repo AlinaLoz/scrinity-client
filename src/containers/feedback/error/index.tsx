7765a1dbfdcc30e0c1c712931c5daf498fbebc88
a.lozyuk <a.lozyuk@pixelplex.io>
2021-10-29 00:42:25 +0300

    [front-6] success/error screens

--- src/containers/feedback/error/index.tsx
+++ src/containers/feedback/error/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useTranslation } from 'next-i18next';
 
-export const ReviewError: React.FC = () => (
-  <div>
-    Error
-    {/* <button>Prev</button> */}
-    {/* <button>Next</button> */}
-  </div>
-);
+import { ERROR_PAGE_IMAGE_PATH } from '@constants/ui.constants';
+import { EmptyPage } from '../components/empty-page';
+
+interface IErrorProps {
+  onNext: () => void;
+}
+
+export const Error: React.FC<IErrorProps> = ({ onNext }) => {
+  const { t } = useTranslation('common');
+
+  return (
+    <EmptyPage
+      onNext={onNext}
+      image={ERROR_PAGE_IMAGE_PATH}
+      title={t('EMPTY_SCREENS.ERROR_TITLE')}
+      description={t('EMPTY_SCREENS.ERROR_DESCRIPTION')}
+    />
+  );
+};
