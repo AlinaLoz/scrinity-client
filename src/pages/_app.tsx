104db9f0e938365664334b484a14ff927429ffbb
a.lozyuk <a.lozyuk@pixelplex.io>
2022-02-04 02:06:16 +0300

    update head title and icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,6 +37,8 @@ const WrappedApp = ({ Component, pageProps }: TWrappedAppProps) => {
   return (
     <>
       <Head>
+        <title>Scrinity</title>
+        <link rel="icon" href="/images/scrinity-icon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
       </Head>
       <ModalContext.Provider value={{
