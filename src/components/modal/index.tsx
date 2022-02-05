04423e18ea303a018602ab5179d79576dc217447
a.lozyuk <a.lozyuk@pixelplex.io>
2022-02-05 17:56:13 +0300

    add useDisableBodyScroll

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -4,10 +4,13 @@ import { MODAL } from '@constants/modal.constants';
 import { ModalContext } from '@contexts/modal.context';
 
 import { FeedbackModal } from '@components/modal/feedback.modal';
+import { useDisableBodyScroll } from '@hooks/use-body-scroll.hooks';
 import { SignInModal } from './sign-in.modal';
 
 export const Modal: React.FC = () => {
   const { modalType } = useContext(ModalContext);
+  useDisableBodyScroll(modalType !== MODAL.NONE);
+
   if (modalType === MODAL.SIGN_IN) {
     return <SignInModal />;
   }
